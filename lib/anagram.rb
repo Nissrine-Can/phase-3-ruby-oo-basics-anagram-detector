d423792be57847664eb556b0879bb78678e8809f
Nissrine Canina <niss.canina@gmail.com>
2021-12-11 21:18:36 -0500

    commit tree cd2ca6231e366ea735ca4a3537f83ccc79a4daa5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match arr
+        arr.filter do |str|
+            str.chars.sort == word.chars.sort
+        end
+    end
+
+end
+
